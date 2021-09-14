da0f4b96b814d20db590e05af9beab23b7597083
Maxim <maximummana.y@gmail.com>
2021-09-14 21:33:03 +0300

    Sqrt and minus fix

--- app/src/main/java/com/maximummana/mycalc/MainActivity.kt
+++ app/src/main/java/com/maximummana/mycalc/MainActivity.kt
@@ -15,9 +15,6 @@ import kotlin.math.sqrt
 import android.R.bool
 
 
-
-
-
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     private var inputData = lazy { StringBuilder() }
@@ -48,28 +45,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        try {
-            if (v is Button)
-                when (v.id) {
+        if (v is Button) {
+            when (v.id) {
 
-                    R.id.button1 -> inputData.value.clear()
+                R.id.button1 -> inputData.value.clear()
 
-                    R.id.button3 -> if (inputData.value.isNotEmpty()) {
-                        inputData.value.deleteAt(inputData.value.lastIndex)
-                    }
+                R.id.button3 -> if (inputData.value.isNotEmpty()) {
+                    inputData.value.deleteAt(inputData.value.lastIndex)
+                }
 
-                    R.id.button4 -> {
+                R.id.button4 -> {
+                    try {
                         historyData.value.append("${inputData.value} = ${Core.calculate(inputData.value.toString())}\n")
                         inputData.value.clear()
                         binding.history.text = historyData.value
+                    } catch (e: Exception) {
+                        Toast.makeText(this, e.message.toString(), Toast.LENGTH_SHORT).show()
                     }
-
-                    else -> inputData.value.append(v.text)
                 }
-            binding.field.text = inputData.value
 
-        } catch (e: Exception) {
-            Toast.makeText(this, e.message.toString(), Toast.LENGTH_SHORT).show()
+                else -> inputData.value.append(v.text)
+            }
+            binding.field.text = inputData.value
         }
     }
 }
@@ -81,11 +78,13 @@ object Core {
 
     private fun getFormat(input: String): String {
 
-        if(!checkBrackets(input)) throw Exception("Error in bracket")
+        if (!checkBrackets(input)) throw Exception("Error in bracket")
 
         val output = StringBuilder()
         val operatorsStack = Stack<Char>()
         var i = 0
+
+
         while (i < input.length) {
 
             if (isDelimiter(input[i])) {
@@ -93,8 +92,12 @@ object Core {
                 continue
             }
 
-            if (Character.isDigit(input[i])) {
-                while (!isDelimiter(input[i]) && !isOperator(input[i])) {
+            var isNegative = input[i] == '-' && (i == 0 || input[i - 1] in "+/*√()")
+
+
+            if (Character.isDigit(input[i]) || (input[i] == '-' && isNegative)) {
+                while (!isDelimiter(input[i]) && (input[i] !in "+/*√()") && !(input[i] == '-' && !isNegative)) {
+                    if (isNegative) isNegative = false
                     output.append(input[i])
                     i++
                     if (i == input.length) break
@@ -104,7 +107,7 @@ object Core {
             }
 
 
-            if (isOperator(input[i])) {
+            if (input[i] in "+/*√()" || (input[i] == '-' && !isNegative)) {
                 when {
                     input[i] == '(' -> operatorsStack.push(input[i])
                     input[i] == ')' -> {
@@ -115,12 +118,17 @@ object Core {
                         }
                     }
                     else -> {
+
                         if (operatorsStack.size > 0) {
                             if (getPriority(input[i]) <= getPriority(operatorsStack.peek())) {
                                 output.append(operatorsStack.pop().toString()).append(" ")
                             }
                         }
-                        operatorsStack.push(input[i])
+                        if (input[i] == '√' && (i == 0 || input[i - 1] in "+-/*()")) operatorsStack.push(
+                            'к'
+                        )
+                        else
+                            operatorsStack.push(input[i])
                     }
                 }
             }
@@ -134,40 +142,36 @@ object Core {
     private fun counting(input: String): Double {
         var result = 0.0
         val temp = Stack<Double>()
-        var i = 0
-        while (i < input.length) {
 
-            if (Character.isDigit(input[i])) {
-                var a = ""
-                while (!isDelimiter(input[i]) && !isOperator(input[i])) {
-                    a += input[i]
-                    i++
-                    if (i == input.length) break
-                }
-                temp.push(a.toDouble())
-                i--
-            } else
-                if (isOperator(input[i])) {
-                    if(temp.empty()) throw Exception("Only operator")
-                    val a = temp.pop()
-                    if(temp.empty()) throw Exception("Only one number")
+        val data = input.dropLast(1).split(" ")
+        for (e in data) {
+            if (e in "+-/*√к()") {
+                if (temp.empty()) throw Exception("Only operator")
+                val a = temp.pop()
+                if (e == "к") {
+                    if (a < 0) throw Exception("Sqrt of a negative number")
+                    result = sqrt(a)
+                } else {
+
+                    if (temp.empty()) throw Exception("Only one number")
                     val b = temp.pop()
-                    when (input[i]) {
-                        '+' -> result = b + a
-                        '-' -> result = b - a
-                        '*' -> result = b * a
-                        '/' -> {
-                            if(a == 0.0) throw Exception("Division by zero")
+                    when (e) {
+                        "+" -> result = b + a
+                        "-" -> result = b - a
+                        "*" -> result = b * a
+                        "/" -> {
+                            if (a == 0.0) throw Exception("Division by zero")
                             result = b / a
                         }
-                        '√' -> {
-                            if(a < 0) throw Exception("Sqrt of a negative number")
+                        "√" -> {
+                            if (a < 0) throw Exception("Sqrt of a negative number")
                             result = b * sqrt(a)
                         }
                     }
-                    temp.push(result)
                 }
-            i++
+                temp.push(result)
+            } else
+                temp.push(e.toDouble())
         }
         return temp.peek()
     }
@@ -184,8 +188,6 @@ object Core {
 
     private fun isDelimiter(c: Char) = c in " "
 
-    private fun isOperator(c: Char) = c in "+-/*√()"
-
     private fun getPriority(s: Char) =
         when (s) {
             '(' -> 0
@@ -195,6 +197,7 @@ object Core {
             '*' -> 4
             '/' -> 4
             '√' -> 5
+            'к' -> 5
             else -> 6
         }
 }
