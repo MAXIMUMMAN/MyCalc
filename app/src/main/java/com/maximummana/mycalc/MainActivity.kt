0af28493d72bd1d4c6f206a78da9b92ce94e0a93
Maxim <maximummana.y@gmail.com>
2021-09-15 20:19:46 +0300

    Переписал логику преобразования в обратную польскую запись и вычислений Попытался задокументировать...

--- app/src/main/java/com/maximummana/mycalc/MainActivity.kt
+++ app/src/main/java/com/maximummana/mycalc/MainActivity.kt
@@ -10,72 +10,111 @@ import androidx.core.view.children
 import com.maximummana.mycalc.databinding.ActivityMainBinding
 import java.lang.Exception
 import java.util.*
-import kotlin.math.pow
 import kotlin.math.sqrt
-import android.R.bool
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var binding: ActivityMainBinding
-    private var inputData = lazy { StringBuilder() }
-    private var historyData = lazy { StringBuilder() }
 
+    private lateinit var binding: ActivityMainBinding
+
+    private var inputData = StringBuilder()
+    private var historyData = StringBuilder()
+
+    /**
+     * Сохраняет данные (P.s. знаю можно и ViewModel
+     */
     override fun onSaveInstanceState(state: Bundle) {
         super.onSaveInstanceState(state)
-        state.putString("inputData", inputData.value.toString())
-        state.putString("historyData", historyData.value.toString())
+
+        state.putString("inputData", inputData.toString())
+        state.putString("historyData", historyData.toString())
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-        inputData.value.clear()
-        inputData.value.append(savedInstanceState.getString("inputData"))
-        binding.field.text = inputData.value
-        historyData.value.clear()
-        historyData.value.append(savedInstanceState.getString("historyData"))
-        binding.history.text = historyData.value
+    /**
+     * Востанавливет данные (P.s. знаю можно и ViewModel
+     */
+    override fun onRestoreInstanceState(state: Bundle) {
+        super.onRestoreInstanceState(state)
+
+        inputData.clear()
+        inputData.append(state.getString("inputData"))
+        binding.field.text = inputData
+
+        historyData.clear()
+        historyData.append(state.getString("historyData"))
+        binding.history.text = historyData
     }
 
+    /**
+     * Делает текст прокручиваемым
+     * и вешает слушатели на кнопки
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
+
         binding.history.movementMethod = ScrollingMovementMethod()
         for (i in binding.keyboard.root.children) i.setOnClickListener(this)
     }
 
+    /**
+     * Каждой кнопке клавиатуры устанавливет действие
+     * @param v View? должен быть кнопкой
+     */
     override fun onClick(v: View?) {
         if (v is Button) {
             when (v.id) {
-
-                R.id.button1 -> inputData.value.clear()
-
-                R.id.button3 -> if (inputData.value.isNotEmpty()) {
-                    inputData.value.deleteAt(inputData.value.lastIndex)
+                R.id.buttonClearAll -> {
+                    inputData.clear()
                 }
 
-                R.id.button4 -> {
+                R.id.buttonClearBack -> {
+                    if (inputData.isNotEmpty()) {
+                        inputData.deleteAt(inputData.lastIndex)
+                    }
+                }
+
+                R.id.buttonCalculate -> {
                     try {
-                        historyData.value.append("${inputData.value} = ${Core.calculate(inputData.value.toString())}\n")
-                        inputData.value.clear()
-                        binding.history.text = historyData.value
+                        historyData.append("$inputData = ${CalcCore.calculate(inputData.toString())}\n")
+                        inputData.clear()
+                        binding.history.text = historyData
                     } catch (e: Exception) {
                         Toast.makeText(this, e.message.toString(), Toast.LENGTH_SHORT).show()
                     }
                 }
 
-                else -> inputData.value.append(v.text)
+                //0..9 and operators
+                else -> inputData.append(v.text)
             }
-            binding.field.text = inputData.value
+            binding.field.text = inputData
         }
     }
 }
 
+/**
+ * @author man-y
+ * Простой класс для вычисления математических выражений.
+ */
+object CalcCore {
 
-object Core {
-
+    /**
+     * Обертка для преобразования и вычислений
+     * @param input String входное математическое выражение (не преобразованное)
+     * @return результат вычислений
+     */
     fun calculate(input: String) = counting(getFormat(input))
 
+
+    /**
+     * Готовит мат. выражение к вычислениям
+     * @throws Exception c разными сообщениями связанными с ошибками в входной строке
+     * @param input String входное математическое выражение (не преобразованное)
+     * @return строку в обратной польской записи
+     */
     private fun getFormat(input: String): String {
 
         if (!checkBrackets(input)) throw Exception("Error in bracket")
@@ -87,17 +126,8 @@ object Core {
 
         while (i < input.length) {
 
-            if (isDelimiter(input[i])) {
-                i++
-                continue
-            }
-
-            var isNegative = input[i] == '-' && (i == 0 || input[i - 1] in "+/*√()")
-
-
-            if (Character.isDigit(input[i]) || (input[i] == '-' && isNegative)) {
-                while (!isDelimiter(input[i]) && (input[i] !in "+/*√()") && !(input[i] == '-' && !isNegative)) {
-                    if (isNegative) isNegative = false
+            if (Character.isDigit(input[i])) {
+                while (input[i] !in "+-/*√()") {
                     output.append(input[i])
                     i++
                     if (i == input.length) break
@@ -107,7 +137,7 @@ object Core {
             }
 
 
-            if (input[i] in "+/*√()" || (input[i] == '-' && !isNegative)) {
+            if (input[i] in "+-/*√()") {
                 when {
                     input[i] == '(' -> operatorsStack.push(input[i])
                     input[i] == ')' -> {
@@ -118,17 +148,20 @@ object Core {
                         }
                     }
                     else -> {
-
-                        if (operatorsStack.size > 0) {
-                            if (getPriority(input[i]) <= getPriority(operatorsStack.peek())) {
-                                output.append(operatorsStack.pop().toString()).append(" ")
+                        when {
+                            input[i] == '√' && (i == 0 || input[i - 1] in "+-м/*()") -> {
+                                changeUnaryOperatorPos("к", operatorsStack, output)
+                                operatorsStack.push('к')
+                            }
+                            input[i] == '-' && (i == 0 || input[i - 1] in "+/*√к()") -> {
+                                changeUnaryOperatorPos("м", operatorsStack, output)
+                                operatorsStack.push('м')
+                            }
+                            else -> {
+                                changeOperatorPos(input[i].toString(), operatorsStack, output)
+                                operatorsStack.push(input[i])
                             }
                         }
-                        if (input[i] == '√' && (i == 0 || input[i - 1] in "+-/*()")) operatorsStack.push(
-                            'к'
-                        )
-                        else
-                            operatorsStack.push(input[i])
                     }
                 }
             }
@@ -139,33 +172,75 @@ object Core {
         return output.toString()
     }
 
+    /**
+     * Раставляет опирации согласно приоритету
+     * @param operator текущий оператор
+     * @param operatorsStack текущий стек с опирациями
+     * @param output String собираемое математическое выражение (в обратной польской записи)
+     * @return true если все без ошибок
+     */
+    private fun changeOperatorPos(operator:String, operatorsStack:Stack<Char>,output:StringBuilder) {
+        if (operatorsStack.size > 0) {
+            if (getPriority(operator) <= getPriority(operatorsStack.peek().toString())) {
+                output.append(operatorsStack.pop().toString()).append(" ")
+            }
+        }
+    }
+
+    /**
+     * Спец. версия для операций с одним аргументом. Раставляет опирации согласно приоритету
+     * @param operator текущий оператор
+     * @param operatorsStack текущий стек с опирациями
+     * @param output String собираемое математическое выражение (в обратной польской записи)
+     * @return true если все без ошибок
+     */
+    private fun changeUnaryOperatorPos(operator:String, operatorsStack:Stack<Char>,output:StringBuilder) {
+        if (operatorsStack.size > 0) {
+            if (getPriority(operator) < getPriority(operatorsStack.peek().toString())) {
+                output.append(operatorsStack.pop().toString()).append(" ")
+            }
+        }
+    }
+
+    /**
+     * Готовит мат. выражение к вычислениям
+     * @param input String входное математическое выражение (в обратной польской записи)
+     * @return double значение выражения
+     */
     private fun counting(input: String): Double {
         var result = 0.0
         val temp = Stack<Double>()
 
         val data = input.dropLast(1).split(" ")
         for (e in data) {
-            if (e in "+-/*√к()") {
+            if (isOperator(e)) {
                 if (temp.empty()) throw Exception("Only operator")
                 val a = temp.pop()
-                if (e == "к") {
-                    if (a < 0) throw Exception("Sqrt of a negative number")
-                    result = sqrt(a)
-                } else {
+                when (e) {
+                    "к" -> {
+                        if (a < 0) throw Exception("Sqrt of a negative number")
+                        result = sqrt(a)
+                    }
+                    "м" -> {
+                        result = -a
+                    }
+                    else -> {
 
-                    if (temp.empty()) throw Exception("Only one number")
-                    val b = temp.pop()
-                    when (e) {
-                        "+" -> result = b + a
-                        "-" -> result = b - a
-                        "*" -> result = b * a
-                        "/" -> {
-                            if (a == 0.0) throw Exception("Division by zero")
-                            result = b / a
-                        }
-                        "√" -> {
-                            if (a < 0) throw Exception("Sqrt of a negative number")
-                            result = b * sqrt(a)
+                        if (temp.empty()) throw Exception("Only one number")
+                        val b = temp.pop()
+
+                        when (e) {
+                            "+" -> result = b + a
+                            "-" -> result = b - a
+                            "*" -> result = b * a
+                            "/" -> {
+                                if (a == 0.0) throw Exception("Division by zero")
+                                result = b / a
+                            }
+                            "√" -> {
+                                if (a < 0) throw Exception("Sqrt of a negative number")
+                                result = b * sqrt(a)
+                            }
                         }
                     }
                 }
@@ -176,6 +251,11 @@ object Core {
         return temp.peek()
     }
 
+    /**
+     * Проверка на правильность скобок
+     * @param input String входное математическое выражение (не преобразованное)
+     * @return true если все без ошибок
+     */
     private fun checkBrackets(input: String): Boolean {
         var brackets = 0
         for (i in input) {
@@ -186,18 +266,26 @@ object Core {
         return brackets == 0
     }
 
-    private fun isDelimiter(c: Char) = c in " "
+    /**
+     * Проверка на оператор
+     * @param s String оператор
+     * @return true если это был оператор
+     */
+    private fun isOperator(s: String) = s in "+-м/*√к()"
 
-    private fun getPriority(s: Char) =
-        when (s) {
-            '(' -> 0
-            ')' -> 1
-            '+' -> 2
-            '-' -> 3
-            '*' -> 4
-            '/' -> 4
-            '√' -> 5
-            'к' -> 5
-            else -> 6
-        }
+    /**
+     * Устанавливает приоритетность операции
+     * @param s String оператор
+     * @return int приоритет
+     */
+    private fun getPriority(s: String) = when (s) {
+        "(" -> 0
+        ")" -> 1
+        "+" -> 2
+        "-" -> 3
+        "*", "/" -> 4
+        "√" -> 5
+        "м","к" -> 6
+        else -> 7
+    }
 }
